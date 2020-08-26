93181bd8a18b7735ceacd68595c30bcb77c0e60e
Vo Thanh Hieu <zannaghazi123>
2020-08-26 16:53:01 +0700

    Remove unlisted code

--- Implementation/test_PTTK/middlewares/session.js
+++
@@ -1,9 +0,0 @@
-var session = require('express-session');
-
-module.exports = function (app) {
-  app.use(session({
-    secret: 'fgzaaflpt20imorsst20',
-    resave: true,
-    saveUninitialized: true
-  }));
-}
--- Implementation/test_PTTK/middlewares/view-engine.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-
-module.exports = function (app) {
-
-app.set("view engine", "ejs");
-app.set("views", "./view");
-}
